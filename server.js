581ac788eb4cfd11055ec44b28cc854e7b2e7507
Joshua Kosasih <joshua.2017tin013@civitas.ukrida.ac.id>
2021-04-04 01:47:02 +0700

    some fix

--- server.js
+++ server.js
@@ -26,4 +26,6 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database Mongoose"));
 
+app.get("/", indexRouter);
+
 app.listen(process.env.PORT || 3000);
